fa95e8ea0d628c4cb84d82f605c08409b0d0c44a
Arash Hatami <hatamiarash7@gmail.com>
2021-08-25 08:57:54 +0430

    add new samples

--- demo.m
+++ demo.m
@@ -1,9 +1,9 @@
 clc
 close all
 
-filename = '1.png';
+filename = '3.png';
 img = imread(filename);
-ref = imread('2.png');
+ref = imread('4.png');
 
 out_image = HistMatch(img, ref);
 filename_ = strcat(filename(1:end-4), '_matched.png');
